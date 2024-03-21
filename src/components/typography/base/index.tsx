c7fa44846410ce19de86492009d3cc08b1435df3
jia-wei-00 <wwwbingo6@gmail.com>
2024-03-22 01:17:11 +0800

    chore: add font family

--- src/components/typography/base/index.tsx
+++ src/components/typography/base/index.tsx
@@ -10,7 +10,11 @@ type THeaders = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 const Base = ({ as: Element = "p", className, ...rest }: TypographyProps) => {
   return (
     <Element
-      className={clsx("typography__color-primary", className)}
+      className={clsx(
+        "typography__base",
+        "typography__color-primary",
+        className
+      )}
       {...rest}
     />
   );
